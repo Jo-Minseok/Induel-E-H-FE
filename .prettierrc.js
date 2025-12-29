0f9c601d4cb4cae11b66d515b17d16fc329a6c72
J_Coder <seok3765@naver.com>
2025-12-29 19:30:43 +0900

    ⚙️ chore: prettier 설정 파일 추가

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all', // 인자값 또는 객체에 마지막 값 또는 속성에 ',' 추가
+  tabWidth: 2, // 탭은 2번씩
+  semi: true, // 마지막 ';' 세미 콜론 추가
+  singleQuote: true, // 모든 문자열은 싱글 쿼터 사용
+  jsxSingleQuote: true, // jsx 안 HTML 내에서 싱글 쿼터 사용
+};
+
+export default config;
