c7134fdc13a9ef073ac6521b2fc163496970e272
J_Coder <seok3765@naver.com>
2025-12-31 02:17:32 +0900

    ⚙️ chore: eslint-plugin-prettier 제거

--- eslint.config.js
+++ eslint.config.js
@@ -4,19 +4,12 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    plugins: {
-      prettier: eslintPluginPrettier,
-    },
-    rules: {
-      'prettier/prettier': 'error', // Prettier 규칙 위반 시 에러
-    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
