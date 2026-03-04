05d93f9488850b9541994486181843f00069cebb
J_Coder <seok3765@naver.com>
2026-03-05 01:52:55 +0900

    ✨ feat: history 위젯 책 제외 HTML 구조 구현

--- src/widgets/history/ui/History.tsx
+++ src/widgets/history/ui/History.tsx
@@ -1,5 +1,28 @@
+import { Fragment } from 'react';
+
+import '../style/History.css';
+
 function History() {
-  return <></>;
+  const indexList = ['List', 'Content', 'History', 'Award'];
+  return (
+    <>
+      <section className='history'>
+        <h2 className='history__title'>History</h2>
+        <ul className='history__category'>
+          {indexList.map((item, index) => (
+            <Fragment key={item}>
+              <li>{item}</li>
+              {index < indexList.length - 1 && <span>|</span>}
+            </Fragment>
+          ))}
+        </ul>
+        <div className='history__book'></div>
+        <div className='history__last'>
+          <p>공간을 문화로 창조하는 기업</p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default History;
