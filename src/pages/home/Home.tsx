160ac2bb5e58c489d699a6912f36437fccc27132
J_Coder <seok3765@naver.com>
2026-03-14 04:54:26 +0900

    ✨ feat: home 페이지에 history 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 // import { Footer } from '@widgets/footer';
 import Hero from '@widgets/hero';
+import History from '@widgets/history';
 
 // import Map from '@widgets/map';
 
@@ -7,6 +8,7 @@ function Home() {
   return (
     <>
       <Hero></Hero>
+      <History></History>
       {/* <Map></Map> */}
       {/* <Footer></Footer> */}
     </>
