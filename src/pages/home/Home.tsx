9c1c08bd893e958fd22177c9c024ec1cbc8a1fc4
J_Coder <seok3765@naver.com>
2026-03-23 23:19:14 +0900

    ⚙️ chore: public api 및 페이지 등록

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,8 @@
 // import { Footer } from '@widgets/footer';
+import Award from '@widgets/award';
 import Hero from '@widgets/hero';
+
+// import History from '@widgets/history';
 
 // import Map from '@widgets/map';
 
@@ -7,8 +10,10 @@ function Home() {
   return (
     <>
       <Hero></Hero>
+      {/* <History></History> */}
       {/* <Map></Map> */}
       {/* <Footer></Footer> */}
+      <Award></Award>
     </>
   );
 }
