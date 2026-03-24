dfa2d67bc2788a8c020a024f37d67ec7afbc88b0
J_Coder <seok3765@naver.com>
2026-03-24 19:37:51 +0900

    ✨ feat: award web 레이아웃 구현

--- src/widgets/award/ui/Award.tsx
+++ src/widgets/award/ui/Award.tsx
@@ -1,14 +1,45 @@
+import { useState } from 'react';
+
+import awards from '../../../entities/award/model/award.json';
+import { YEAR_LIST } from '../model/constant';
+import { getAwardImage } from '../model/helper';
 import '../styles/Award.css';
+import { Card } from './Card';
+import { Pagination } from './Pagination';
 import { AwardTitle } from './Title';
 import { YearCategory } from './YearCategory';
 
+const ITEMS_PER_PAGE = 5;
+
 function Award() {
+  const [page, setPage] = useState(0);
+  const totalPages = Math.ceil(awards.length / ITEMS_PER_PAGE);
+  const currentAwards = awards.slice(
+    page * ITEMS_PER_PAGE,
+    (page + 1) * ITEMS_PER_PAGE,
+  );
+
   return (
     <section className='award'>
       <div className='award__top'>
         <AwardTitle></AwardTitle>
-        <YearCategory></YearCategory>
+        <YearCategory yearList={YEAR_LIST}></YearCategory>
       </div>
+      <div className='award__card_wrapper'>
+        {currentAwards.map((award) => (
+          <Card
+            key={award.id}
+            name={award.title}
+            year={award.time}
+            imageUrl={getAwardImage(award.id)}
+          />
+        ))}
+      </div>
+      <Pagination
+        currentPage={page}
+        totalPages={totalPages}
+        onPageChange={setPage}
+      />
     </section>
   );
 }
