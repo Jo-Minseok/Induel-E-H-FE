6a815e2df5f172255094572005afb0dddb374775
J_Coder <seok3765@naver.com>
2026-03-28 02:25:46 +0900

    🧹 clean: light house 횟수 수정

--- .lighthouserc.mobile.cjs
+++ .lighthouserc.mobile.cjs
@@ -3,7 +3,7 @@ module.exports = {
     collect: {
       staticDistDir: './dist',
       url: ['/'],
-      numberOfRuns: 5,
+      numberOfRuns: 3,
     },
     upload: {
       target: 'temporary-public-storage',
