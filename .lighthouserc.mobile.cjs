f98f360f802803ccbe46c7274643ddf87bd02899
J_Coder <seok3765@naver.com>
2026-03-28 00:56:11 +0900

    👹 fix: three.js WebGL 렌더링이 CI의 Headless Chrome에서 GPU 없이 실패 문제

--- .lighthouserc.mobile.cjs
+++ .lighthouserc.mobile.cjs
@@ -3,7 +3,10 @@ module.exports = {
     collect: {
       staticDistDir: './dist',
       url: ['/'],
-      numberOfRuns: 5,
+      numberOfRuns: 3,
+      settings: {
+        chromeFlags: '--no-sandbox --disable-gpu --disable-dev-shm-usage',
+      },
     },
     upload: {
       target: 'temporary-public-storage',
