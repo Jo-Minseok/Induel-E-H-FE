4bb503e6379781e253bca0b70da76d177b160196
J_Coder <seok3765@naver.com>
2026-03-29 06:12:01 +0900

    ✨ feat: 카드 카테고리 추가

--- src/widgets/award/ui/Card.tsx
+++ src/widgets/award/ui/Card.tsx
@@ -1,22 +1,20 @@
+import type { AwardItem } from '@entities/award';
+
+import { getAwardImage } from '../model/helper';
 import '../styles/Card.css';
 
-export function Card({
-  name,
-  year,
-  imageUrl,
-}: {
-  name: string;
-  year: string;
-  imageUrl: string;
-}) {
+export function Card({ award }: { award: AwardItem }) {
   return (
     <div className='award__card'>
       <div className='award__card_image'>
-        <img src={imageUrl} alt={name} loading='lazy' />
+        <img src={getAwardImage(award.id)} alt={award.title} loading='lazy' />
       </div>
-      <div className='award__card_title'>
-        <h4>{name}</h4>
-        <p>{year.slice(0, 4)}</p>
+      <div className='award__card_content'>
+        <div className='award__card_text'>
+          <p className='award__card_category'>{award.category}</p>
+          <h4 className='award__card_title'>{award.title}</h4>
+        </div>
+        <p className='award__card_year'>{award.date.slice(0, 4)}</p>
       </div>
     </div>
   );
